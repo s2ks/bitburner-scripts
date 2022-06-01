1c1454b5b1d4798d4dd44b6729413ed4530be209
s2ks <bakkerjilles@gmail.com>
2022-06-01 22:48:57 +0200

    rename to something sane

--- game/startup.js
+++ game/startup.js
@@ -1,7 +1,7 @@
 export async function main(ns) {
 	const startup = {
-		"milker-master.js": {
-			args: ["--hackprog", "/milker/hack.js", "--weakprog", "/milker/weaken.js", "--growprog", "/milker/grow.js"],
+		"harvester-master.js": {
+			args: ["--hackprog", "/harvester/hack.js", "--weakprog", "/harvester/weaken.js", "--growprog", "/harvester/grow.js"],
 		},
 		"hacknet-master.js": {args: []},
 		"rootkit.js": {args: []},

55bfbd6fa92a1d56377ecfa1b09b70f3f0a156d3
s2ks <bakkerjilles@gmail.com>
2022-06-04 23:26:59 +0200

    latest

---
+++ game/config.js
@@ -0,0 +1,8 @@
+/* TODO Define some constants here */
+export const config = {
+	/* Port numbers */
+	ports: {
+		SERVER_MASTER_SND: 1,
+		STOCK_MASTER_SND: 2,
+	},
+};
--- game/stock-master.js
+++ game/stock-master.js
@@ -1,3 +1,30 @@
 export async function main(ns) {
-	ns.tprint("hello");
+	const syms = ns.stock.getSymbols();
+	const player = ns.getPlayer();
+
+
+
+	if(player.hasTixApiAccess == false) {
+		ns.tprint(`TIX API not available`);
+		ns.exit();
+	}
+
+	if(player.has4SDataTixApi == false) {
+		ns.tprint(`4S Market Data TIX API not available`);
+		ns.exit();
+	}
+
+
+
+	for(const sym of syms) {
+		/* Requires 4S */
+		//ns.stock.getForecase(sym);
+		//ns.stock.getVolatility(sym);
+
+		const shares = ns.stock.getMaxShares(sym);
+		const cost = ns.stock.getPurchaseCost(sym, shares, "Long");
+		const sale = ns.stock.getSaleGain(sym, shares, "Long");
+		ns.tprint(`${sym} cost: ${ns.nFormat(cost, '0.00a')} ${ns.nFormat(sale, '0.00a')} profit: ${ns.nFormat(sale - cost, '0.00a')}` +
+		` forecast: ${ns.stock.getForecast(sym)} volatility: ${ns.stock.getVolatility(sym)}`);
+	}
 }

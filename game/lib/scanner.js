37262bf4716912c716d8f954df7b764bf75b65df
s2ks <bakkerjilles@gmail.com>
2022-06-04 12:56:52 +0200

    cleanup

--- game/lib/scanner.js
+++ game/lib/scanner.js
@@ -17,57 +17,30 @@ export async function scanner(ns, host, path, callback) {
 	}
 }
 
-async function scanner2(ns, host, path, callback) {
-	var names = ns.scan(host);
-	var p = path.slice();
+export async function netscan(ns, action) {
+	const doScan = async (host, path) => {
+		const names = ns.scan(host);
+		const p = path.slice();
 
-	await callback(host, p);
+		await action(host, p);
 
-	p.push(host);
+		p.push(host);
 
-	for(const name of names) {
-		if(p.includes(name) == false) {
-			await scanner2(ns, name, p, callback);
+		for(const name of names) {
+			if(p.includes(name) == false) {
+				await doScan(host, p);
+			}
 		}
 	}
-}
 
-/** @param {NS} ns */
-export async function netscan(ns, action) {
-	return await scanner2(ns, ns.getHostname(), [], action);
+	return await doScan(ns.getHostname(), []);
 }
 
 export async function getAllServers(ns) {
-	var servers = [];
+	const servers = [];
 	await netscan(ns, host => {
 		servers.push(host);
 	});
 
 	return servers;
-}
-
-/** @param {NS} ns */
-function printinfo(ns, host, path) {
-	var portReq 	= ns.getServerNumPortsRequired(host);
-	var lvlReq 	= ns.getServerRequiredHackingLevel(host);
-	var hasRoot 	= ns.hasRootAccess(host);
-	var ram 	= ns.getServerMaxRam(host);
-	var maxMoney 	= ns.getServerMaxMoney(host);
-	var moneyAvail 	= ns.getServerMoneyAvailable(host);
-	var weakentime 	= ns.getWeakenTime(host);
-	var hacktime 	= ns.getHackTime(host);
-	var growtime 	= ns.getGrowTime(host);
-
-	ns.tprintf("%s:", host);
-	ns.tprintf("\t-Root: %s", hasRoot ? "Yes" : "No");
-	ns.tprintf("\t-Hack requirement: open ports [%d] / hacker level  [%d]", portReq, lvlReq);
-	ns.tprintf("\t-Server max RAM: %d GB", ram);
-	ns.tprintf("\t-Balance %f%%", (moneyAvail / maxMoney) * 100);
-	ns.tprintf("\t-Hack time / Weaken time / Grow time: %f / %f / %f", hacktime, weakentime, growtime);
-	ns.tprintf("\t Origin: %v", path);
-}
-
-/** @param {NS} ns */
-export async function main(ns) {
-	await scanner(ns, ns.getHostname(), [], printinfo);
 }
